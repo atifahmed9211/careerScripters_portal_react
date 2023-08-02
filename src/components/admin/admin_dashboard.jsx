d36a0e3869b6e269d92a00d4450640c7471db76a
atif ahmed <atif.ahmed9211@gmail.com>
2023-08-02 09:33:36 +0500

    2-8-2023

--- src/components/admin/admin_dashboard.jsx
+++ src/components/admin/admin_dashboard.jsx
@@ -55,7 +55,7 @@ function AdminDashboard() {
                     <div className="col-sm-12 col-xl-6">
                         <div className="bg-light text-center rounded p-4">
                             <div className="d-flex align-items-center justify-content-between mb-4">
-                                <h6 className="mb-0">Worldwide Sales</h6>
+                                <h6 className="mb-0">Worlwide Sales</h6>
                                 <a href="">Show All</a>
                             </div>
                             <canvas id="worldwide-sales"></canvas>
